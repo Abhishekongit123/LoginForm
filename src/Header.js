b1ed1a8e6f74b9f1098d4553d6cca724a0d81e47
Abhishekongit123 <manojkumae50013@gmail.com>
2021-09-01 08:16:26 +0530

    Login Form

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function header() {
+    return(
+        <header> <h1 className="mld">Veer Bahadur Singh Purvanchal University,Jaunpur,UP,222001</h1></header>
+    );
+    
+}
+export default header;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import MenuIcon from '@material-ui/icons/Menu';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+	<>
+	<App />
+	</>,
+	document.getElementById('root')
+);
